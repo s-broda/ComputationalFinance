f41e834b13ddf5fb42d32191ae9611650aca91a2
Simon Broda <s.a.broda@uva.nl>
2017-12-07 18:10:11 +0100

    layout

--- slides/pdf/makeslides.sh
+++ slides/pdf/makeslides.sh
@@ -6,8 +6,8 @@ for (( i=1; i<=6; i++))
 	do
 		addr=${jnserver:0:22}notebooks/week$i.ipynb${jnserver:22}
 		`npm bin`/decktape -s 1680x1050 rise $addr week$i.pdf
+		pdfnup --nup 2x2 --suffix '2x2' --batch week$i.pdf
 	done
 kill $!
-pdfnup --nup 2x2 --suffix '2x2' --batch week?.pdf
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -r150 -sOutputFile=ComputationalFinance.pdf week?-2x2.pdf
 rm week?-2x2.pdf
